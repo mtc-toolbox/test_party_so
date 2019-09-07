aa454adb8607540b0a72ca2ff7cc668fdd6b0e27
Рындин Михаил Анатолиевич <mryndin@stream.ru>
2019-09-07 16:53:05 +0300

    format Apple model

--- common/models/Apple.php
+++ common/models/Apple.php
@@ -7,12 +7,12 @@ use Yii;
 /**
  * This is the model class for table "Apple".
  *
- * @property int $Id Идентификатор записи
- * @property int $Color Цвет яблока
+ * @property int    $Id               Идентификатор записи
+ * @property int    $Color            Цвет яблока
  * @property string $IntegrityPercent Процент целостности
- * @property string $CreatedAt Время появления
- * @property string $FalledAt Время падения
- * @property string $DeletedAt Время полного поедания
+ * @property string $CreatedAt        Время появления
+ * @property string $FalledAt         Время падения
+ * @property string $DeletedAt        Время полного поедания
  */
 class Apple extends \yii\db\ActiveRecord
 {
@@ -42,12 +42,12 @@ class Apple extends \yii\db\ActiveRecord
     public function attributeLabels()
     {
         return [
-            'Id' => Yii::t('app', 'Идентификатор записи'),
-            'Color' => Yii::t('app', 'Цвет яблока'),
+            'Id'               => Yii::t('app', 'Идентификатор записи'),
+            'Color'            => Yii::t('app', 'Цвет яблока'),
             'IntegrityPercent' => Yii::t('app', 'Процент целостности'),
-            'CreatedAt' => Yii::t('app', 'Время появления'),
-            'FalledAt' => Yii::t('app', 'Время падения'),
-            'DeletedAt' => Yii::t('app', 'Время полного поедания'),
+            'CreatedAt'        => Yii::t('app', 'Время появления'),
+            'FalledAt'         => Yii::t('app', 'Время падения'),
+            'DeletedAt'        => Yii::t('app', 'Время полного поедания'),
         ];
     }
 }
