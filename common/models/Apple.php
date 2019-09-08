ce21367e46c5cb6e4e4d54fe28f8e991849c00a0
Рындин Михаил Анатолиевич <mryndin@stream.ru>
2019-09-08 08:11:06 +0300

    Add apple model

--- common/models/Apple.php
+++ common/models/Apple.php
@@ -3,6 +3,9 @@
 namespace common\models;
 
 use Yii;
+use yii\base\Exception;
+use yii\db\ActiveRecord;
+use yii\behaviors\TimestampBehavior;
 
 /**
  * This is the model class for table "Apple".
@@ -16,6 +19,8 @@ use Yii;
  */
 class Apple extends \yii\db\ActiveRecord
 {
+    const TIME_TO_BAD_STATE = 3600 * 5;
+
     // наименование цветов яблок
     const COLOR_NAME_GREEN  = 'green';
     const COLOR_NAME_RED    = 'red';
@@ -28,12 +33,38 @@ class Apple extends \yii\db\ActiveRecord
         self::COLOR_NAME_YELLOW => 2,
     ];
 
+    const REVERSE_COLOR_MAP = [
+        self::COLOR_NAME_GREEN,
+        self::COLOR_NAME_RED,
+        self::COLOR_NAME_YELLOW,
+    ];
+
     /**
      * {@inheritdoc}
      */
     public static function tableName()
     {
         return 'Apple';
+    }
+
+    /**
+     * Apple constructor.
+     *
+     * @param string|null $color
+     * @param array       $config
+     *
+     * @throws Exception
+     */
+    public function __construct(string $color = null, $config = [])
+    {
+        parent::__construct($config);
+
+        if (isset($color)) {
+            $this->Color = self::COLOR_MAP[$color];
+            if (!isset($this->Color)) {
+                throw new Exception(Yii::t('app', 'Apple color is bad'), 500);
+            }
+        }
     }
 
     /**
@@ -61,5 +92,76 @@ class Apple extends \yii\db\ActiveRecord
             'FalledAt'         => Yii::t('app', 'Время падения'),
             'DeletedAt'        => Yii::t('app', 'Время полного поедания'),
         ];
+    }
+
+    /**
+     * @return array
+     */
+    public function behaviors()
+    {
+        return [
+            'timestamp' => [
+                'class'      => TimestampBehavior::className(),
+                'attributes' => [
+                    ActiveRecord::EVENT_BEFORE_INSERT => ['CreatedAt'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param float $percent
+     *
+     * @return $this
+     * @throws Exception
+     */
+    public function eat(float $percent)
+    {
+        if (isset($this->DeletedAt)) {
+            throw new Exception(Yii::t('app', 'Apple alredy eated'), 403);
+        }
+
+        if (!isset($this->FalledAt)) {
+            throw new Exception(Yii::t('app', 'Apple not falled'), 403);
+        }
+
+        if ($this->isBad()) {
+            throw new Exception(Yii::t('app', 'Apple already bad'), 403);
+        }
+
+        $this->IntegrityPercent = round($this->IntegrityPercent - $this->IntegrityPercent * $percent / 100, 2);
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isBad()
+    {
+        return (isset($this->FalledAt) && (($this->FalledAt + self::TIME_TO_BAD_STATE)>=time()));
+    }
+
+    /**
+     * @return $this
+     * @throws Exception
+     */
+    public function fall()
+    {
+        if (isset($this->FalledAt)) {
+            throw new Exception(Yii::t('app', 'Apple already falled'), 403);
+        }
+
+        $this->FalledAt = time();
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getColorName()
+    {
+        return self::REVERSE_COLOR_MAP[$this->Color];
     }
 }
