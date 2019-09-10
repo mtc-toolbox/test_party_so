a839adc52f3f748141e16738cbc75c3750e13d6e
Рындин Михаил Анатолиевич <mryndin@stream.ru>
2019-09-10 06:07:16 +0300

    Add AppleHelpers, change php minimal version

--- common/helpers/AppleCommonHelper.php
+++ common/helpers/AppleCommonHelper.php
@@ -2,20 +2,48 @@
 
 namespace common\helpers;
 
+use common\models\Apple;
+use yii\db\Connection;
+
 class AppleCommonHelper
 {
 
     const MIN_GENERATED_COUNT = 10;
     const MAX_GENERATED_COUNT = 100;
 
+    /**
+     * @param int $minCount
+     * @param int $maxCount
+     *
+     * @return bool
+     * @throws \yii\base\Exception
+     */
     public static function generateApples(int $minCount = AppleCommonHelper::MIN_GENERATED_COUNT, int $maxCount = AppleCommonHelper::MAX_GENERATED_COUNT)
     {
-        $models = [];
-
         $count = rand($minCount, $maxCount);
 
-        for ($i = 0; $i < $count; $i++) {
+        try {
+            $transaction = static::getDb()->beginTransaction();
+
+            for ($i = 0; $i <= $count; $i++) {
+                $model = new Apple();
+                if (!$model->save()) {
+                    $transaction->rollBack();
+
+                    return false;
+                }
+            }
+
+            $transaction->commit();
+
+        } catch (Exception $e) {
+            throw $e;
+        } catch (Throwable $e) {
+            $transaction->rollBack();
+            throw $e;
         }
+
+        return true;
 
     }
 }
