f518ffa58bc887270903c5da9561e18f2009288b
Рындин Михаил Анатолиевич <mryndin@stream.ru>
2019-09-10 15:19:00 +0300

    Add WS server generate methods

--- console/daemons/CommandsServer.php
+++ console/daemons/CommandsServer.php
@@ -3,29 +3,158 @@
 
 namespace app\daemons;
 
-use consik\yii2websocket\events\WSClientEvent;
+use common\helpers\AppleCommonHelper;
+use common\models\Apple;
+use console\models\SocketSession;
 use consik\yii2websocket\WebSocketServer;
 use Ratchet\ConnectionInterface;
+use yii\helpers\Console;
+use yii\helpers\Json;
 
-class CommandsServer  extends WebSocketServer
+use console\helpers\WSCommonHelper;
+
+/**
+ * Class CommandsServer
+ * @package console\daemons
+ */
+class CommandsServer extends WebSocketServer
 {
-    public function init()
-    {
-        parent::init();
+    /**
+     * @var \SplObjectStorage
+     */
+    protected $sessions;
 
-        $this->on(self::EVENT_CLIENT_CONNECTED, function(WSClientEvent $e) {
-            $e->
-            $e->client->name = null;
-        });
+    /**
+     * CommandsServer constructor.
+     */
+    public function __construct()
+    {
+        $this->sessions = new \SplObjectStorage();
+
+        Console::stdout('WS server started' . PHP_EOL);
     }
 
+    /**
+     * @param ConnectionInterface $conn
+     */
+    public function onOpen(ConnectionInterface $conn)
+    {
+        Console::stdout('Connection Open' . PHP_EOL);
 
+        $session = new SocketSession($conn);
+
+        $this->sessions->attach($session);
+    }
+
+    /**
+     * @param ConnectionInterface $from
+     * @param                     $msg
+     *
+     * @return string|null
+     */
     protected function getCommand(ConnectionInterface $from, $msg)
     {
-        $request = json_decode($msg, true);
-        return !empty($request['action']) ? $request['action'] : parent::getCommand($from, $msg);
+        $currentCommand = static::getCommandName($msg) ?? parent::getCommand($from, $msg);
+        Console::stdout("Connection get command {$currentCommand}" . PHP_EOL);
+
+        return $currentCommand;
     }
 
+    /**
+     * @param string $message
+     *
+     * @return string|null
+     */
+    protected function getCommandName(string $message)
+    {
+        $request = Json::decode($message);
 
+        return $request['action'] ?? null;
+    }
 
+    /**
+     * @param ConnectionInterface $client
+     * @param                     $msg
+     *
+     * @throws \yii\base\Exception
+     */
+    protected function commandGenerate(ConnectionInterface $client, $msg)
+    {
+        $session = $this->findSession($client);
+
+        if (!isset($session)) {
+            $session->sendState(WSCommonHelper::STATUS_DENIED, 'Session not found');
+            $session->close();
+            $this->deleteSession($session);
+
+            return;
+        }
+
+        if ($session->isGuest()) {
+            $session->sendState(WSCommonHelper::STATUS_DENIED, 'Access denied');
+            $session->close();
+            $this->deleteSession($session);
+
+            return;
+        }
+
+        if (!AppleCommonHelper::generateApples()) {
+            $session->sendState(WSCommonHelper::STATUS_UNKNOWN, 'Apples generate error');
+
+            return;
+        }
+
+        /* @var SocketSession $session */
+        foreach ($this->sessions as $session) {
+            $session->sendRefresh('Need to refresh browser');
+        }
+
+    }
+
+    /**
+     * @param ConnectionInterface $from
+     *
+     * @return SocketSession|object|null
+     */
+    protected function findSession(ConnectionInterface $from)
+    {
+        /* @var SocketSession $session */
+        foreach ($this->sessions as $session) {
+            $client = $session->getClient();
+            if ($client === $from) {
+                return $session;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * @param SocketSession $session
+     */
+    protected function deleteSession(SocketSession $session)
+    {
+        /* @var SocketSession $session */
+        $this->sessions->detach($session);
+    }
+
+    /**
+     * @param ConnectionInterface $from
+     *
+     * @return array
+     */
+    protected function getOtherSessions(ConnectionInterface $from)
+    {
+        $result = [];
+
+        /* @var SocketSession $session */
+        foreach ($this->sessions as $session) {
+            $client = $session->getClient();
+            if ($client !== $from) {
+                $result[] = $session;
+            }
+        }
+
+        return $result;
+    }
 }
--- console/models/SocketSession.php
+++ console/models/SocketSession.php
@@ -3,7 +3,7 @@
 
 namespace console\models;
 
-use consik\yii2websocket\events\WSClientEvent;
+use console\helpers\WSCommonHelper;
 use Yii;
 use Ratchet\ConnectionInterface;
 use yii\base\Model;
@@ -11,18 +11,115 @@ use common\models\User;
 
 class SocketSession extends Model
 {
+    const SESSION_TTL = 10 * 60;
+
     /**
-     * @var WSClientEvent $connection
+     * @var string
      */
+    public $action;
+
+    /**
+     * @var string
+     */
+    public $token;
+
+    /**
+     * @var array
+     */
+    public $data;
+    /**
+     * @var ConnectionInterface $connection
+     */
+
     protected $client;
 
-    public function __construct(WSClientEvent $client, $config = [])
+    /**
+     * SocketSession constructor.
+     *
+     * @param ConnectionInterface|null $client
+     * @param array                    $config
+     */
+    public function __construct(ConnectionInterface $client = null, $config = [])
     {
         parent::__construct($config);
         $this->client = $client;
     }
 
-    protected function getUser($tokens)
+    /**
+     * @return ConnectionInterface
+     */
+    public function getClient()
     {
+        return $this->client;
+    }
+
+    /**
+     * @param $client
+     *
+     * @return $this
+     */
+    public function setClient($client)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isGuest()
+    {
+        $user = $this->getUser();
+
+        return !isset($user);
+    }
+
+    /**
+     * @param int    $state
+     * @param string $msg
+     *
+     * @return ConnectionInterface
+     */
+    public function sendState(int $state, string $msg)
+    {
+        $data = WSCommonHelper::buildStateMessage($state, $msg);
+
+        return $this->client->send($data);
+    }
+
+    /**
+     * @param string $msg
+     *
+     * @return ConnectionInterface
+     */
+    public function sendRefresh(string $msg = '')
+    {
+        $data = WSCommonHelper::buildMessage(WSCommonHelper::STATUS_OK, $msg, WSCommonHelper::ACTION_REFRESH);
+
+        return $this->client->send($data);
+    }
+
+    public function close()
+    {
+        $this->client->close();
+    }
+
+    /**
+     * @return User|\yii\web\IdentityInterface|null
+     */
+    protected function getUser()
+    {
+        $cache = Yii::$app->cache;
+
+        $userId = $cache->get($this->token);
+
+        if ($userId === false) {
+            return null;
+        }
+
+        $cache->set($userId, static::SESSION_TTL);
+
+        return User::findIdentity($userId);
     }
 }
