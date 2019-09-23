09597f41e52015fd7cbf364cdec5c3a0e0530815
Mikhail Ryndin <mryndin@stream.ru>
2019-09-23 11:56:37 +0300

    Fix Cache

--- common/config/main.php
+++ common/config/main.php
@@ -7,9 +7,22 @@ return [
     'language'   => 'ru-RU',
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        /*
         'cache' => [
             'class'     => 'yii\caching\FileCache',
             'cachePath' => '@backend/cache',
         ],
+        */
+        'cache' => [
+            'class'   => 'yii\caching\MemCache',
+            'servers' => [
+                [
+                    'host'   => 'localhost',
+                    'port'   => 11211,
+                    'weight' => 100,
+                ],
+            ],
+        ],
+
     ],
 ];
